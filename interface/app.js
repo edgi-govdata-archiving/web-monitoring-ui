da1123cccaadf344c44f4e7e75d9898846638be7
Kevin Nguyen <kevin.nguyen@lineophile.com>
2017-02-24 02:46:33 -0800

    Modify port to work with heroku

--- interface/app.js
+++ interface/app.js
@@ -107,6 +107,6 @@ app.get('/diffbyindex?', function(req, res) {
     });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Listening on port 3000')
 });
