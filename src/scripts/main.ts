51912bd7c4de951ba03ca1133761b08e47c76a6b
Kevin Nguyen <kevin.nguyen@lineophile.com>
2017-03-13 00:07:33 -0700

    Comment code

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -8,10 +8,6 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-$( document ).ready(function() {
-    toggleProgressbar(false);
-})
-
 function loadIframe(html_embed: string) {
     // inject html
     var iframe = document.getElementById('diff_view');
@@ -30,12 +26,4 @@ function loadIframe(html_embed: string) {
         // set iframe height = frame content
         iframe.setAttribute('height',(iframe as any).contentWindow.document.body.scrollHeight);
     };
-}
-
-function toggleProgressbar(isVisible: boolean) {
-    if(isVisible) {
-        $('.progress').show();
-    } else {
-        $('.progress').hide();
-    }
 }
