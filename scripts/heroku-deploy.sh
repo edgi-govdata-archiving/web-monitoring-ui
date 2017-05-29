c8385063a35f02593bde9a53afa507db57d88824
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2017-05-28 17:22:32 -0700

    Control flow

--- scripts/heroku-deploy.sh
+++ scripts/heroku-deploy.sh
@@ -1,3 +1,30 @@
 #!/bin/bash -e
 
-echo $1
+# Takes 2 parameters
+# ${1} - deploy branch name
+# ${2} - heroku remote name
+
+(
+# Check and set defaults
+if [ -z "${1}" ]; then
+    localBranch="heroku-deploy"
+else
+    localBranch=${1}
+fi
+
+if [ -z "${2}" ]; then
+    remote="heroku"
+else
+    remote=${2}
+fi
+
+echo ${localBranch}
+echo ${remote}
+
+exists=`git show-ref refs/heads/${1}`
+if [ -n "$exists" ]; then
+    echo "git checkout ${1}"
+else
+    echo "git checkout -b ${1}"
+fi
+)
