83664a6078e9cec90dbf218581e5d4fa18c875bc
lazywithclass <montecristoh@gmail.com>
2017-06-06 16:49:18 +0200

    added an example, working, test for navbar

---
+++ src/components/__tests__/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {shallow} from 'enzyme';
+import NavBar from '../nav-bar';
+
+test('navbar holds title and username', () => {
+  const navbar = shallow(<NavBar title="ohhai" user="me" />);
+  expect(navbar.find('Link').node.props.children).toEqual('ohhai')
+  expect(navbar.find('ul > li').text()).toEqual('me')
+});
