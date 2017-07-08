76f53a8a5f5ea30708ce86b63d54fddcfa2f0e31
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-07-08 16:27:00 -0400

    plumbing selectDiffType

--- src/components/select-diffType.tsx
+++ src/components/select-diffType.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
 
+export interface DiffTypes {
+  [propName: string]: string;
+}
+
+export const diffTypes:DiffTypes = {
+  HIGHLIGHTED_TEXT: 'highlighted text',
+  HIGHLIGHTED_SOURCE: 'highlighted source',
+  HIGLIGHTED_RENDERED: 'higlighted rendered', 
+  SIDE_BY_SIDE_RENDERED: 'side-by-side rendered', 
+  SIDE_BY_SIDE_TEXT: 'side-by-side text',
+}
+
 export default class SelectDiffType extends React.Component<any, any> {
+
     render () {
-        const diffTypes = this.props.diffTypes;
+        // const diffTypes = this.props.diffTypes;
+        const handleChange = (e: any) => {
+          this.props.onChange(e.target.value);
+        }
+
         return (
-            <select>
-                {diffTypes.map((diffType: any) => <option key={diffType} value={diffType}>{diffType}</option>)};
+            <select value={this.props.value} onChange={handleChange}>
+              {Object.keys(diffTypes).map((key) => {
+                var val = diffTypes[key];
+                return <option key={key} value={key}>{val}</option>
+              })}
+              {/*diffTypes.map((diffType: any) => <option key={diffType} value={diffType}>{diffType}</option>)};*/}
             </select>
         );
     }
