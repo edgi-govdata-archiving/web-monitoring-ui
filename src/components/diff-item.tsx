4088f8c4eab2fdbfcab6ac447671e8ee2d167659
danielballan <daniel.b.allan@gmail.com>
2017-07-10 10:59:59 -0400

    got diff requests working and UI expecting the right format

--- src/components/diff-item.tsx
+++ src/components/diff-item.tsx
@@ -6,28 +6,31 @@ export default class DiffItem extends React.Component<any,any> {
   render() {
     const { data, onSelect } = this.props;
     const diff = data;
-    
+
     // here we do inline style-editing and class switching.
     // in the real world it's probably a good idea to associate
     // styles with the class and remove the "styles" object entirely.
     let styles:any = {};
     let diffTypeClass:string = "unchanged";
-    
+    var itemType:number;
+    var itemText:string;
+    [itemType, itemText] = data;
+
     // this data comes from https://github.com/edgi-govdata-archiving/go-calc-diff
     // it may be necessary to adjust the "data.Type" inspections based on differ
-    if (diff.Type == -1) {
+    if (itemType == -1) {
       diffTypeClass = "removed"
       styles.background = "red";
-    } else if (diff.Type == 1) {
+    } else if (itemType == 1) {
       diffTypeClass = "added"
       styles.background = "green";
     }
 
     return (
-      <span 
+      <span
         className={`diff item ${diffTypeClass}`}
         style={styles}
-        onClick={onSelect}>{diff.Text}</span>
+        onClick={onSelect}>{itemText}</span>
     );
   }
 }
--- src/constants/DiffTypes.tsx
+++ src/constants/DiffTypes.tsx
@@ -5,8 +5,8 @@ export interface DiffTypes {
 export const diffTypes:DiffTypes = {
   HIGHLIGHTED_TEXT: 'highlighted text',
   HIGHLIGHTED_SOURCE: 'highlighted source',
-  HIGLIGHTED_RENDERED: 'higlighted rendered', 
-  SIDE_BY_SIDE_RENDERED: 'side-by-side rendered', 
+  HIGLIGHTED_RENDERED: 'higlighted rendered',
+  SIDE_BY_SIDE_RENDERED: 'side-by-side rendered',
   SIDE_BY_SIDE_TEXT: 'side-by-side text',
 }
 
