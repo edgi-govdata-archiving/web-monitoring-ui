06b2d783b74910b8f4208acd1dcd91a3ffd907c9
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2017-07-13 13:48:10 -0700

    Finish tooltips

--- src/components/annotation-form.tsx
+++ src/components/annotation-form.tsx
@@ -1,5 +1,6 @@
 /* tslint:disable:max-line-length */
 import * as React from 'react';
+// Using this component I found: https://github.com/mcumpl/react-lightweight-tooltip
 import {Tooltip} from 'react-lightweight-tooltip';
 import {Version} from '../services/web-monitoring-db';
 
@@ -42,10 +43,10 @@ export default class AnnotationForm extends React.Component<IAnnotationFormProps
             gap: {},
             tooltip: {
                 background: '#444',
-                borderRadius: 3,
-                hack: 'hide'
+                borderRadius: 3
             },
             wrapper: {
+                cursor: 'default',
                 display: 'block'
             }
         };
@@ -79,23 +80,47 @@ export default class AnnotationForm extends React.Component<IAnnotationFormProps
                     <div className="signifier-container">
                         <h5>Repeated Changes</h5>
                         <ul className="signifier-list">
-                            <Checkbox {...common} name="repeat_7">Header menu removal or change</Checkbox>
-                            <Checkbox {...common} name="repeat_8">Template text, page format, or comment field removal or change</Checkbox>
-                            <Checkbox {...common} name="repeat_9">Footer or site map removal or change</Checkbox>
-                            <Checkbox {...common} name="repeat_10">Sidebar removal or change</Checkbox>
-                            <Checkbox {...common} name="repeat_11">Banner/advertisement removal or change</Checkbox>
-                            <Checkbox {...common} name="repeat_12">Scrolling news/reports</Checkbox>
+                            <Tooltip content="Header menu removal or change" styles={tooltipStyles}>
+                                <Checkbox {...common} name="repeat_7">Header menu removal or change</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Template text, page format, or comment field removal or change" styles={tooltipStyles}>
+                                <Checkbox {...common} name="repeat_8">Template text, page format, or comment field removal or change</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Footer or site map removal or change" styles={tooltipStyles}>
+                                <Checkbox {...common} name="repeat_9">Footer or site map removal or change</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Sidebar removal or change" styles={tooltipStyles}>
+                                <Checkbox {...common} name="repeat_10">Sidebar removal or change</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Banner/advertisement removal or change" styles={tooltipStyles}>
+                                <Checkbox {...common} name="repeat_11">Banner/advertisement removal or change</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Scrolling news/reports" styles={tooltipStyles}>
+                                <Checkbox {...common} name="repeat_12">Scrolling news/reports</Checkbox>
+                            </Tooltip>
                         </ul>
                     </div>
                     <div className="signifier-container">
                         <h5>Significance</h5>
                         <ul className="signifier-list">
-                            <Checkbox {...common} name="sig_1">Change related to energy, environment, or climate</Checkbox>
-                            <Checkbox {...common} name="sig_2">Language is significantly altered</Checkbox>
-                            <Checkbox {...common} name="sig_3">Content is removed</Checkbox>
-                            <Checkbox {...common} name="sig_4">Page is removed</Checkbox>
-                            <Checkbox {...common} name="sig_5">Insignificant</Checkbox>
-                            <Checkbox {...common} name="sig_6">Repeated Insignificant</Checkbox>
+                            <Tooltip content="Change related to energy, environment, or climate" styles={tooltipStyles}>
+                                <Checkbox {...common} name="sig_1">Change related to energy, environment, or climate</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Language is significantly altered" styles={tooltipStyles}>
+                                <Checkbox {...common} name="sig_2">Language is significantly altered</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Content is removed" styles={tooltipStyles}>
+                                <Checkbox {...common} name="sig_3">Content is removed</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Page is removed" styles={tooltipStyles}>
+                                <Checkbox {...common} name="sig_4">Page is removed</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Insignificant" styles={tooltipStyles}>
+                                <Checkbox {...common} name="sig_5">Insignificant</Checkbox>
+                            </Tooltip>
+                            <Tooltip content="Repeated Insignificant" styles={tooltipStyles}>
+                                <Checkbox {...common} name="sig_6">Repeated Insignificant</Checkbox>
+                            </Tooltip>
                         </ul>
                     </div>
                 </form>
@@ -127,12 +152,7 @@ function Checkbox ({children, formValues, name, onChange}: ICheckboxProps) {
         <li className="signifier-list-item">
             <input type="checkbox" className="toggle" id={name} checked={checked} onChange={changeHandler}/>
             <label className="toggle-btn" data-tg-on={fieldNumber} htmlFor={name} />
-            <span className="info-text">= {children}</span>
+            <span className="info-text">{children}</span>
         </li>
-//         <label>
-// -            <input type="checkbox" name={name} checked={checked} onChange={changeHandler} />
-// -            {fieldNumber}
-// -            <span className="info-text">= {children}</span>
-// -        </label>
     );
 }
