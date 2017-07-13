72a91520547e5d5bdabd4820ef76c45b8a5d4e67
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2017-07-12 17:58:31 -0700

    New button style

--- src/components/annotation-form.tsx
+++ src/components/annotation-form.tsx
@@ -33,31 +33,37 @@ export default class AnnotationForm extends React.Component<IAnnotationFormProps
         return (
             <form className={classes.join(' ')}>
                 <div className="col-md-4">
-                    <h4>Individual Page Changes</h4>
-                    <Checkbox {...common} name="indiv_1">Date and time change only</Checkbox>
-                    <Checkbox {...common} name="indiv_2">Text or numeric content removal or change</Checkbox>
-                    <Checkbox {...common} name="indiv_3">Image content removal or change</Checkbox>
-                    <Checkbox {...common} name="indiv_4">Hyperlink removal or change</Checkbox>
-                    <Checkbox {...common} name="indiv_5">Text-box, entry field, or interactive component removal or change</Checkbox>
-                    <Checkbox {...common} name="indiv_6">Page removal (whether it has happened in the past or is currently removed)</Checkbox>
+                    <h5>Individual Page Changes</h5>
+                    <ul className="signifier-list">
+                        <Checkbox {...common} name="indiv_1">Date and time change only</Checkbox>
+                        <Checkbox {...common} name="indiv_2">Text or numeric content removal or change</Checkbox>
+                        <Checkbox {...common} name="indiv_3">Image content removal or change</Checkbox>
+                        <Checkbox {...common} name="indiv_4">Hyperlink removal or change</Checkbox>
+                        <Checkbox {...common} name="indiv_5">Text-box, entry field, or interactive component removal or change</Checkbox>
+                        <Checkbox {...common} name="indiv_6">Page removal (whether it has happened in the past or is currently removed)</Checkbox>
+                    </ul>
                 </div>
                 <div className="col-md-4">
-                    <h4>Repeated Changes</h4>
-                    <Checkbox {...common} name="repeat_7">Header menu removal or change</Checkbox>
-                    <Checkbox {...common} name="repeat_8">Template text, page format, or comment field removal or change</Checkbox>
-                    <Checkbox {...common} name="repeat_9">Footer or site map removal or change</Checkbox>
-                    <Checkbox {...common} name="repeat_10">Sidebar removal or change</Checkbox>
-                    <Checkbox {...common} name="repeat_11">Banner/advertisement removal or change</Checkbox>
-                    <Checkbox {...common} name="repeat_12">Scrolling news/reports</Checkbox>
+                    <h5>Repeated Changes</h5>
+                    <ul className="signifier-list">
+                        <Checkbox {...common} name="repeat_7">Header menu removal or change</Checkbox>
+                        <Checkbox {...common} name="repeat_8">Template text, page format, or comment field removal or change</Checkbox>
+                        <Checkbox {...common} name="repeat_9">Footer or site map removal or change</Checkbox>
+                        <Checkbox {...common} name="repeat_10">Sidebar removal or change</Checkbox>
+                        <Checkbox {...common} name="repeat_11">Banner/advertisement removal or change</Checkbox>
+                        <Checkbox {...common} name="repeat_12">Scrolling news/reports</Checkbox>
+                    </ul>
                 </div>
                 <div className="col-md-4">
-                    <h4>Significance</h4>
-                    <Checkbox {...common} name="sig_1">Change related to energy, environment, or climate</Checkbox>
-                    <Checkbox {...common} name="sig_2">Language is significantly altered</Checkbox>
-                    <Checkbox {...common} name="sig_3">Content is removed</Checkbox>
-                    <Checkbox {...common} name="sig_4">Page is removed</Checkbox>
-                    <Checkbox {...common} name="sig_5">Insignificant</Checkbox>
-                    <Checkbox {...common} name="sig_6">Repeated Insignificant</Checkbox>
+                    <h5>Significance</h5>
+                    <ul className="signifier-list">
+                        <Checkbox {...common} name="sig_1">Change related to energy, environment, or climate</Checkbox>
+                        <Checkbox {...common} name="sig_2">Language is significantly altered</Checkbox>
+                        <Checkbox {...common} name="sig_3">Content is removed</Checkbox>
+                        <Checkbox {...common} name="sig_4">Page is removed</Checkbox>
+                        <Checkbox {...common} name="sig_5">Insignificant</Checkbox>
+                        <Checkbox {...common} name="sig_6">Repeated Insignificant</Checkbox>
+                    </ul>
                 </div>
             </form>
         );
@@ -83,12 +89,15 @@ function Checkbox ({children, formValues, name, onChange}: ICheckboxProps) {
     const checked = !!(formValues && formValues[name]);
     const changeHandler = (event: React.FormEvent<HTMLInputElement>) =>
         onChange({[name]: event.currentTarget.checked});
-
     return (
-        <label>
-            <input type="checkbox" name={name} checked={checked} onChange={changeHandler} />
-            {fieldNumber}
-            <span className="info-text">= {children}</span>
-        </label>
+        <li className="signifier-list-item">
+            <input type="checkbox" className="tgl tgl-skewed" id={name} checked={checked} onChange={changeHandler}/>
+            <label className="tgl-btn" data-tg-off={fieldNumber} data-tg-on={fieldNumber} htmlFor={name} />
+        </li>
+//         <label>
+// -            <input type="checkbox" name={name} checked={checked} onChange={changeHandler} />
+// -            {fieldNumber}
+// -            <span className="info-text">= {children}</span>
+// -        </label>
     );
 }
