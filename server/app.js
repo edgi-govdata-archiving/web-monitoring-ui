c9994c7fe5d43b4da6bbd50f51f10f6347f0c725
Rob Brackett <rob@robbrackett.com>
2017-07-27 18:26:26 -0700

    Add env var to force SSL for all requests

--- server/app.js
+++ server/app.js
@@ -7,14 +7,25 @@ const sheetData = require('./sheet-data');
 const config = require('./configuration');
 
 const serverPort = process.env.PORT || 3001;
+const filterArray = [
+    'WEB_MONITORING_DB_URL'
+];
+
+if (process.env.FORCE_SSL && process.env.FORCE_SSL.toLowerCase() === 'true') {
+    app.use((request, response, next) => {
+        if (request.secure || request.headers['x-forwarded-proto'] === 'https') {
+            return next();
+        }
+        response.redirect(
+            301,
+            `https://${request.headers.host}${request.originalUrl}`
+        );
+    });
+}
 
 app.set('views', path.join(__dirname, '../views'));
 app.use(express.static('dist'));
 app.engine('html', require('ejs').renderFile);
-
-const filterArray = [
-    'WEB_MONITORING_DB_URL'
-];
 
 app.get('/api/domains/:username', function(request, response) {
     const username = request.params.username;
