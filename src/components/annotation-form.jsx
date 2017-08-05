11177e1ef7415964136742d2675b1b19550342f4
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2017-08-04 21:36:40 -0700

    Remove redundant form container

--- src/components/annotation-form.jsx
+++ src/components/annotation-form.jsx
@@ -56,91 +56,87 @@ export default class AnnotationForm extends React.Component {
             }
         };
 
-        // FIXME: should probably remove .annotation-inputs, since it is
-        // redundant with the <form>
         return (
-            <div className="annotation-inputs">
-                <form className={classes.join(' ')}>
-                    <div className="annotation-form__signifiers">
-                    <div className="signifier-container">
-                        <h5>Individual Page Changes</h5>
-                        <ul className="signifier-list">
-                            <Tooltip content="Date and time change only" styles={tooltipStyles}>
-                                <Checkbox {...common} name="indiv_1">Date and time change only</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Text or numeric content removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="indiv_2">Text or numeric content removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Image content removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="indiv_3">Image content removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Hyperlink removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="indiv_4">Hyperlink removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Text-box, entry field, or interactive component removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="indiv_5">Text-box, entry field, or interactive component removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Page removal (whether it has happened in the past or is currently removed)" styles={tooltipStyles}>
-                                <Checkbox {...common} name="indiv_6">Page removal (whether it has happened in the past or is currently removed)</Checkbox>
-                            </Tooltip>
-                        </ul>
-                    </div>
-                    <div className="signifier-container">
-                        <h5>Repeated Changes</h5>
-                        <ul className="signifier-list">
-                            <Tooltip content="Header menu removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="repeat_7">Header menu removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Template text, page format, or comment field removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="repeat_8">Template text, page format, or comment field removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Footer or site map removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="repeat_9">Footer or site map removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Sidebar removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="repeat_10">Sidebar removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Banner/advertisement removal or change" styles={tooltipStyles}>
-                                <Checkbox {...common} name="repeat_11">Banner/advertisement removal or change</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Scrolling news/reports" styles={tooltipStyles}>
-                                <Checkbox {...common} name="repeat_12">Scrolling news/reports</Checkbox>
-                            </Tooltip>
-                        </ul>
-                    </div>
-                    <div className="signifier-container">
-                        <h5>Significance</h5>
-                        <ul className="signifier-list">
-                            <Tooltip content="Change related to energy, environment, or climate" styles={tooltipStyles}>
-                                <Checkbox {...common} name="sig_1">Change related to energy, environment, or climate</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Language is significantly altered" styles={tooltipStyles}>
-                                <Checkbox {...common} name="sig_2">Language is significantly altered</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Content is removed" styles={tooltipStyles}>
-                                <Checkbox {...common} name="sig_3">Content is removed</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Page is removed" styles={tooltipStyles}>
-                                <Checkbox {...common} name="sig_4">Page is removed</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Insignificant" styles={tooltipStyles}>
-                                <Checkbox {...common} name="sig_5">Insignificant</Checkbox>
-                            </Tooltip>
-                            <Tooltip content="Repeated Insignificant" styles={tooltipStyles}>
-                                <Checkbox {...common} name="sig_6">Repeated Insignificant</Checkbox>
-                            </Tooltip>
-                        </ul>
-                    </div>
-                    </div>
+            <form className={classes.join(' ')}>
+                <div className="annotation-form__signifiers">
+                <div className="signifier-container">
+                    <h5>Individual Page Changes</h5>
+                    <ul className="signifier-list">
+                        <Tooltip content="Date and time change only" styles={tooltipStyles}>
+                            <Checkbox {...common} name="indiv_1">Date and time change only</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Text or numeric content removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="indiv_2">Text or numeric content removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Image content removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="indiv_3">Image content removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Hyperlink removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="indiv_4">Hyperlink removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Text-box, entry field, or interactive component removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="indiv_5">Text-box, entry field, or interactive component removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Page removal (whether it has happened in the past or is currently removed)" styles={tooltipStyles}>
+                            <Checkbox {...common} name="indiv_6">Page removal (whether it has happened in the past or is currently removed)</Checkbox>
+                        </Tooltip>
+                    </ul>
+                </div>
+                <div className="signifier-container">
+                    <h5>Repeated Changes</h5>
+                    <ul className="signifier-list">
+                        <Tooltip content="Header menu removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="repeat_7">Header menu removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Template text, page format, or comment field removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="repeat_8">Template text, page format, or comment field removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Footer or site map removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="repeat_9">Footer or site map removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Sidebar removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="repeat_10">Sidebar removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Banner/advertisement removal or change" styles={tooltipStyles}>
+                            <Checkbox {...common} name="repeat_11">Banner/advertisement removal or change</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Scrolling news/reports" styles={tooltipStyles}>
+                            <Checkbox {...common} name="repeat_12">Scrolling news/reports</Checkbox>
+                        </Tooltip>
+                    </ul>
+                </div>
+                <div className="signifier-container">
+                    <h5>Significance</h5>
+                    <ul className="signifier-list">
+                        <Tooltip content="Change related to energy, environment, or climate" styles={tooltipStyles}>
+                            <Checkbox {...common} name="sig_1">Change related to energy, environment, or climate</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Language is significantly altered" styles={tooltipStyles}>
+                            <Checkbox {...common} name="sig_2">Language is significantly altered</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Content is removed" styles={tooltipStyles}>
+                            <Checkbox {...common} name="sig_3">Content is removed</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Page is removed" styles={tooltipStyles}>
+                            <Checkbox {...common} name="sig_4">Page is removed</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Insignificant" styles={tooltipStyles}>
+                            <Checkbox {...common} name="sig_5">Insignificant</Checkbox>
+                        </Tooltip>
+                        <Tooltip content="Repeated Insignificant" styles={tooltipStyles}>
+                            <Checkbox {...common} name="sig_6">Repeated Insignificant</Checkbox>
+                        </Tooltip>
+                    </ul>
+                </div>
+                </div>
 
-                    <textarea
-                        name="further-notes"
-                        placeholder="Further notes"
-                        onChange={this._onNotesChange}
-                        value={annotation.notes || ''}
-                    />
-                </form>
-            </div>
+                <textarea
+                    name="further-notes"
+                    placeholder="Further notes"
+                    onChange={this._onNotesChange}
+                    value={annotation.notes || ''}
+                />
+            </form>
         );
     }
 
