b2585c83ef8dc08baf5a76d75a75e1ebc21cd2a6
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2017-09-06 23:05:32 -0700

    Change diffTypes description to title case

--- src/constants/diff-types.js
+++ src/constants/diff-types.js
@@ -1,36 +1,36 @@
 export const diffTypes = {
   HIGHLIGHTED_TEXT: {
-    description: 'highlighted text',
+    description: 'Highlighted Text',
     diff_service: 'html_text',
     value: 'HIGHLIGHTED_TEXT'
   },
   HIGHLIGHTED_SOURCE: {
-    description: 'highlighted source',
+    description: 'Highlighted Source',
     diff_service: 'html_source',
     value: 'HIGHLIGHTED_SOURCE'
   },
   HIGHLIGHTED_RENDERED: {
-    description: 'highlighted rendered',
+    description: 'Highlighted Rendered',
     diff_service: 'TODO',
     value: 'HIGHLIGHTED_RENDERED'
   },
   SIDE_BY_SIDE_RENDERED: {
-    description: 'side-by-side rendered',
+    description: 'Side-by-Side Rendered',
     diff_service: 'html_source',  // HACK, real side-by-side doesn't exist yet
     value: 'SIDE_BY_SIDE_RENDERED'
   },
   SIDE_BY_SIDE_TEXT: {
-    description: 'side-by-side text',
+    description: 'Side-by-Side Text',
     diff_service: 'TODO',
     value: 'SIDE_BY_SIDE_TEXT'
   },
   CHANGES_ONLY_TEXT: {
-    description: 'changes only text',
+    description: 'Changes Only Text',
     diff_service: 'html_text',
     value: 'CHANGES_ONLY_TEXT'
   },
   CHANGES_ONLY_SOURCE: {
-    description :'changes only source',
+    description :'Changes Only Source',
     diff_service: 'html_source',
     value: 'CHANGES_ONLY_SOURCE'
   }
