0efe538ebd653b45b11ef6108a45cc6904383e12
Rob Brackett <rob@robbrackett.com>
2017-09-08 12:16:34 -0700

    Include some context in the changes-only diffs
    For now, trimmed context is represented by a simple ellipsis. In the future, we might want some more custom rendering.
    
    We trim to 3 lines of context or, if the context is on a single line, trim that line to 300 characters. Those values are for either side of the context, i.e. 3 lines/300 chars after a change and another 3 lines/300 chars before a change.

--- src/components/changes-only-diff.jsx
+++ src/components/changes-only-diff.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import DiffItem from './diff-item';
 import List from './list';
 
+// The context for a change should be constrained to this many lines
+const maxContextLines = 3;
+// If the context is only a single line, it should be constrained to this length
+const maxContextLineLength = 300;
+
 /**
  * Display a plaintext diff with additions and removals inline.
  *
@@ -17,9 +22,7 @@ export default class ChangesOnlyDiff extends React.Component {
       return null;
     }
 
-    const changesOnly = this.props.diff.content.diff.filter(currentValue => {
-      return currentValue[0] !== 0;
-    });
+    const changesOnly = this.props.diff.content.diff.map(getContextualDiff);
 
     return (
       <List
@@ -32,28 +35,58 @@ export default class ChangesOnlyDiff extends React.Component {
 }
 
 
-/** TODO: Putting contextual diffs off until after v0
-   * Truncating function to process changes-only diffs.
-   * If `currentValue` is an insertion or deletion we return it as is.
-   * If it's not, we truncate it depending on if changes occured in entries before or after it.
-   * @param {array|Object} currentValue
-   * @param {number} index
-   * @param {diff[]} diffs
-   * @returns {[number, string]}
-   */
-// function getContextualDiff (currentValue, index, diffs) {
-//   let [itemType, itemText] = currentValue;
-//   if (itemType !== 0) return currentValue;
+/**
+ * Trim parts of diff entries where there are no changes to show only a few
+ * lines or characters on the ends adjacent to actual changes.
+ * If `currentValue` is an insertion or deletion we return it as is.
+ * If it's not, we truncate it depending on if changes occured in entries before or after it.
+ * @param {[number, string]} currentValue
+ * @param {number} index
+ * @param {Array.<[number, string]>} diffs
+ * @returns {[number, string]}
+ */
+function getContextualDiff (currentValue, index, diffs) {
+  let [itemType, itemText] = currentValue;
+  if (itemType !== 0) return currentValue;
 
-//   // Naive approach, needs massive improvement
-//   const contextLength = 50;
-//   let strContext = '';
+  // Determine whether there is content that actually needs trimming
+  let lines = itemText.split('\n');
+  const singleLine = lines.length === 1;
+  if (!singleLine && lines.length <= maxContextLines) return currentValue;
 
-//   if (diffs[index - 1] && diffs[index - 1][0] !== 0) {
-//     strContext += `${itemText.substring(0, contextLength)}\n`;
-//   }
-//   if (diffs[index + 1] && diffs[index + 1][0] !== 0) {
-//     strContext += `\n${itemText.substring(itemText.length - contextLength)}`;
-//   }
-//   return [itemType, strContext];
-// }
+  const hasPreviousChange = diffs[index - 1] && diffs[index - 1][0] !== 0;
+  const hasNextChange = diffs[index + 1] && diffs[index + 1][0] !== 0;
+
+  let contextLength = singleLine ? maxContextLineLength : maxContextLines;
+  if (hasPreviousChange && hasNextChange) {
+    contextLength *= 2;
+  }
+  if ((singleLine ? lines[0] : lines).length <= contextLength) {
+    return currentValue;
+  }
+
+  // ...and actually do the trimming
+  let newText = [];
+  if (hasPreviousChange) {
+    if (singleLine) {
+      newText.push(lines[0].slice(0, maxContextLineLength));
+    }
+    else {
+      newText.push(lines.slice(0, maxContextLines).join('\n') + '\n');
+    }
+  }
+
+  // Just divide with an ellipsis for now, could be fancier in the future
+  newText.push('…');
+
+  if (hasNextChange) {
+    if (singleLine) {
+      newText.push(lines[0].slice(-maxContextLineLength));
+    }
+    else {
+      newText.push('\n' + lines.slice(-maxContextLines).join('\n'));
+    }
+  }
+
+  return [itemType, newText.join('')];
+}
