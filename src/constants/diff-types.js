3eadee665424fe11439283c883434799595f408e
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2017-09-07 22:26:40 -0700

    Populate diffTypes 'value' properties programmatically

--- src/constants/diff-types.js
+++ src/constants/diff-types.js
@@ -2,36 +2,33 @@ export const diffTypes = {
   HIGHLIGHTED_TEXT: {
     description: 'Highlighted Text',
     diffService: 'html_text',
-    value: 'HIGHLIGHTED_TEXT'
   },
   HIGHLIGHTED_SOURCE: {
     description: 'Highlighted Source',
     diffService: 'source',
-    value: 'HIGHLIGHTED_SOURCE'
   },
   HIGHLIGHTED_RENDERED: {
     description: 'Highlighted Rendered',
     diffService: 'TODO',
-    value: 'HIGHLIGHTED_RENDERED'
   },
   SIDE_BY_SIDE_RENDERED: {
     description: 'Side-by-Side Rendered',
     diffService: 'html_source',  // HACK, real side-by-side doesn't exist yet
-    value: 'SIDE_BY_SIDE_RENDERED'
   },
   SIDE_BY_SIDE_TEXT: {
     description: 'Side-by-Side Text',
     diffService: 'TODO',
-    value: 'SIDE_BY_SIDE_TEXT'
   },
   CHANGES_ONLY_TEXT: {
     description: 'Changes Only Text',
     diffService: 'html_text',
-    value: 'CHANGES_ONLY_TEXT'
   },
   CHANGES_ONLY_SOURCE: {
     description: 'Changes Only Source',
     diffService: 'source',
-    value: 'CHANGES_ONLY_SOURCE'
   }
 };
+
+for (let key in diffTypes) {
+  diffTypes[key].value = key;
+}
