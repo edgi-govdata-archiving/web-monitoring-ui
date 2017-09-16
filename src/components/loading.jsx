505f8ea8adeb6ce01a15c8385d6bcc3a182403d0
lazywithclass <montecristoh@gmail.com>
2017-09-16 20:50:39 +0200

    dont use image background, use an img instead

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -7,6 +7,6 @@ export default class Loading extends React.Component {
   }
 
   render () {
-    return (<div className="loading">&nbsp;</div>);
+    return (<img src="/img/infinity-loader.svg" className="loading" />);
   }
 }
