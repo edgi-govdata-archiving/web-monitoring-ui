7559ce9fafba60e5377e405bda1fb78d625194a3
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2017-10-09 11:52:44 -0700

    Add error notification

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,37 @@ const gulp = require('gulp');
 const babelify = require('babelify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
+const gutil = require('gulp-util');
+const notify = require('gulp-notify');
 
-gulp.task('default', ['css', 'browserify']);
+const reportError = function (error) {
+  notify({
+    title: 'Task Failed [browserify]',
+    message: 'See console.',
+    sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+  }).write(error);
+
+  gutil.beep();
+
+  // Pretty error reporting
+  let report = '';
+  const chalk = gutil.colors.white.bgRed;
+  const lineNumber = error.loc.line;
+
+  report += chalk('TASK:') + ' browserify\n';
+  report += chalk('PROB:') + ` ${error.message}\n`;
+  report += chalk('LINE:') + ` ${lineNumber}\n`;
+  console.error(report); // eslint-disable-line
+
+  // Prevent the 'watch' task from stopping
+  this.emit('end');
+};
+
+gulp.task('default', ['css', 'img', 'browserify']);
 
 gulp.task('css', function () {
   return gulp.src('src/css/*').pipe(gulp.dest('dist/css'));
 });
-
-gulp.task('default', ['css', 'img', 'browserify']);
 
 gulp.task('img', function () {
   return gulp.src('src/img/*').pipe(gulp.dest('dist/img'));
@@ -26,6 +49,7 @@ gulp.task('browserify', function () {
   })
     .transform(babelify)
     .bundle()
+    .on('error', reportError)
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('dist'));
 });
@@ -35,3 +59,4 @@ gulp.task('watch', function() {
   gulp.watch('src/**/*.css', ['css']);
   gulp.watch('src/img/*', ['img']);
 });
+
