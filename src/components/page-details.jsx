e13835bd89886ae9fb105848f5a930f7443d1964
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2018-04-18 21:59:36 -0700

    Turn cutoff date into constant

--- src/components/page-details.jsx
+++ src/components/page-details.jsx
@@ -233,9 +233,10 @@ export default class PageDetails extends React.Component {
      * https://github.com/edgi-govdata-archiving/web-monitoring-ui/pull/98
      * Issue outlined here: https://github.com/edgi-govdata-archiving/web-monitoring-db/issues/264
      */
+    const cutoffDate = '2016-11-01';
     Promise.resolve(fromList || this.context.api.getPage(pageId))
       .then((page) => {
-        this._loadVersions(page, '2016-11-01', '');
+        this._loadVersions(page, cutoffDate, '');
       });
   }
 
