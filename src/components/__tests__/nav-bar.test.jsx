c624ff30c54e891d49762790ef2c59b350b293d3
Rob Brackett <rob@robbrackett.com>
2018-09-01 23:01:29 -0700

    Update React (and related libraries) to v16
    Fixes #197.
    Supersedes #232, #251, #255, #258, #261, #262.
    
    This just upgrades our dependencies and keeps things working, but does not update our usage to take advantage of new React features.

--- src/components/__tests__/nav-bar.test.jsx
+++ src/components/__tests__/nav-bar.test.jsx
@@ -6,6 +6,6 @@ import NavBar from '../nav-bar';
 
 test('navbar holds title and username', () => {
   const navbar = shallow(<NavBar title="ohhai" user={{email: 'me'}} />);
-  expect(navbar.find('Link').node.props.children).toEqual('ohhai');
+  expect(navbar.find('Link').getElement().props.children).toEqual('ohhai');
   expect(navbar.find('ul > li').last().text()).toEqual('me (Log out)');
 });
