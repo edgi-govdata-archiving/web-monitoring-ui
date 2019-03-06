a57ebd50cb1c844fb33f60f378890dce16ca1601
Mariana Lungu <lun.mariancik@gmail.com>
2019-03-06 15:41:58 -0500

    Add fallback for Intl api #155 (#229)
    When we initially added the issue about needed a fallback for `Intl.DateTimeFormat`, the second-latest version of Safari was still reasonably common on Mac and needed support. Since then, we've had another major version of Safari released, and browsers that don't support this feature are comparatively rare. So this simplifies our fallback by not using a full polyfill, and instead just using `Date.toLocaleString()`, a method that's been around for more than a decade and has pretty universal support. It won't have quite the same formatting we want, but it will produce a readable, localized date, and we'll only wind up falling back it rarely.
    
    Doing this means we don't incur any overhead for shipping a full date localization polyfill.
    
    Thanks to @marlungu for the initial work here!

--- src/scripts/formatters.js
+++ src/scripts/formatters.js
@@ -1,14 +1,27 @@
 /*eslint-env commonjs*/
 
-exports.dateFormatter = new Intl.DateTimeFormat('en-US', {
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  month: 'long',
-  second: 'numeric',
-  year: 'numeric',
-  timeZoneName: 'short'
-});
+// Intl.DateTimeFormat is available in most engines, but not *everywhere.*
+if (global.Intl && global.Intl.DateTimeFormat) {
+  exports.dateFormatter = new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    month: 'long',
+    second: 'numeric',
+    year: 'numeric',
+    timeZoneName: 'short'
+  });
+}
+else {
+  // This follows the same basic API as Intl.DateTimeFormat, but returns a very
+  // simple old-school localized date. It won't have perfect formatting, but it
+  // works and is minimally complex.
+  exports.dateFormatter = {
+    format (date) {
+      return date.toLocaleString();
+    }
+  };
+}
 
 exports.formatMaintainers = maintainers => maintainers.map(maintainership => maintainership.name).join(', ');
 
