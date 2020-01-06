6325e8e44a5909dd0d4a911c304587dff6481b96
g-merrill <44042970+g-merrill@users.noreply.github.com>
2020-01-05 16:22:10 -0800

    Add url to page title (#435)
    This shows the original URL of the page being diffed in the title bar/browser tab when viewing the PageDetails view. It also updates the title to be "Scanner" instead of "web-monitoring-ui".
    
    Fixes #193.

---
+++ src/components/__tests__/page-details.test.jsx
@@ -0,0 +1,52 @@
+/* eslint-env jest */
+import PageDetails from '../page-details';
+import React from 'react';
+import {shallow} from 'enzyme';
+import simplePage from '../../__mocks__/simple-page.json';
+import WebMonitoringDb from '../../services/web-monitoring-db';
+
+describe('page-details', () => {
+  // Change string values to date objects so they're parsed correctly
+  simplePage.versions.forEach(version => {
+    version.capture_time = new Date(version.capture_time);
+  });
+  const match = { params: { pageId: simplePage.uuid }};
+  const createMockApi = () => {
+    return Object.assign(Object.create(WebMonitoringDb.prototype), {
+      getPage: jest.fn().mockResolvedValue(simplePage),
+      getVersions: jest.fn().mockResolvedValue(simplePage.versions)
+    });
+  };
+
+  it('can render', () => {
+    const mockApi = createMockApi();
+    const pageDetails = shallow(
+      <PageDetails
+        match={match}
+      />,
+      {context: {api: mockApi}}
+    );
+
+    expect(pageDetails.exists()).toEqual(true);
+    // It should always have rendered *something.*
+    expect(pageDetails.get(0)).toBeTruthy();
+  });
+
+  it('shows correct title', async () => {
+    const mockApi = createMockApi();
+    const pageDetails = shallow(
+      <PageDetails
+        match={match}
+      />,
+      {context: {api: mockApi}}
+    );
+
+    await mockApi.getPage.mock.results[0].value;
+    await mockApi.getVersions.mock.results[0].value;
+
+    expect(document.title).toBe('Scanner | http://www.ncei.noaa.gov/news/earth-science-conference-convenes');
+
+    pageDetails.unmount();
+    expect(document.title).toBe('Scanner');
+  });
+});
