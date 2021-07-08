ca0b26997d0ea63dead50cdc39f4b2a411bbefb0
Rob Brackett <rob@robbrackett.com>
2021-07-08 12:58:01 -0700

    Add tests

---
+++ src/components/__tests__/page-url-details.test.jsx
@@ -0,0 +1,115 @@
+/* eslint-env jest */
+import PageUrlDetails from '../page-url-details/page-url-details';
+import React from 'react';
+import { mount } from 'enzyme';
+import simplePage from '../../__mocks__/simple-page.json';
+
+describe('PageUrlDetails Component', () => {
+  // Change string values to date objects so they're parsed correctly
+  simplePage.versions.forEach(version => {
+    version.capture_time = new Date(version.capture_time);
+  });
+
+  it('shows nothing if version URLs and redirects match the page URL', () => {
+    const view = mount(
+      <PageUrlDetails
+        page={simplePage}
+        from={simplePage.versions[1]}
+        to={simplePage.versions[0]}
+      />
+    );
+
+    expect(view.children()).toHaveLength(0);
+  });
+
+  it("shows the versions' URL if it differs from the page's", () => {
+    const version1 = {
+      ...simplePage.versions[1],
+      url: `${simplePage.url}/something`
+    };
+    const version2 = {
+      ...simplePage.versions[0],
+      url: `${simplePage.url}/something`
+    };
+    const view = mount(
+      <PageUrlDetails
+        page={simplePage}
+        from={version1}
+        to={version2}
+      />
+    );
+
+    expect(view.children()).toHaveLength(1);
+    expect(view.find('ExternalLink')).toHaveLength(1);
+    expect(view.contains(<ins>/ something</ins>)).toEqual(true);
+  });
+
+  it("shows the versions' redirects", () => {
+    const version1 = {
+      ...simplePage.versions[1],
+      source_metadata: {
+        ...simplePage.versions[1].source_metadata,
+        redirects: [
+          simplePage.url,
+          `${simplePage.url}/something`
+        ]
+      }
+    };
+    const version2 = {
+      ...simplePage.versions[0],
+      source_metadata: {
+        ...simplePage.versions[0].source_metadata,
+        redirects: [
+          simplePage.url,
+          `${simplePage.url}/something`
+        ]
+      }
+    };
+    const view = mount(
+      <PageUrlDetails
+        page={simplePage}
+        from={version1}
+        to={version2}
+      />
+    );
+
+    expect(view.children()).toHaveLength(1);
+    expect(view.find('ExternalLink')).toHaveLength(2);
+    expect(view.contains(<ins>/ something</ins>)).toEqual(true);
+  });
+
+  it('shows separate URL histories for each version if they differ', () => {
+    const version1 = {
+      ...simplePage.versions[1],
+      source_metadata: {
+        ...simplePage.versions[1].source_metadata,
+        redirects: [
+          simplePage.url,
+          `${simplePage.url}/something`
+        ]
+      }
+    };
+    const version2 = {
+      ...simplePage.versions[0],
+      source_metadata: {
+        ...simplePage.versions[0].source_metadata,
+        redirects: [
+          simplePage.url,
+          `${simplePage.url}/something/else`
+        ]
+      }
+    };
+    const view = mount(
+      <PageUrlDetails
+        page={simplePage}
+        from={version1}
+        to={version2}
+      />
+    );
+
+    expect(view.children()).toHaveLength(1);
+    expect(view.find('ExternalLink')).toHaveLength(4);
+    expect(view.contains(<ins>/ something</ins>)).toEqual(true);
+    expect(view.contains(<ins>/ else</ins>)).toEqual(true);
+  });
+});
