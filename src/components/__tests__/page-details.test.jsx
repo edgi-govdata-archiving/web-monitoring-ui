266d42e3e6312b038ec5c439f41de6eb28059856
Rob Brackett <rob@robbrackett.com>
2025-02-12 07:31:50 -0800

    Ensure we always show the versions in the URL (#1064)
    Way back when we added version sampling to speed things up and reduce overhead, it came with the downside that some links from our our task sheets wouldn't always display as expected, because they might not include one of the sampled versions. They would then wind up displaying a different change. This has become a much bigger issue recently, where we have a lot of spurious, possibly bad captures from various sources (mostly Wayback), and the cost of switching to a different version may be displaying something drastically different.
    
    This solves the issue by explicitly loading the expected versions and merging them into the sample if they aren’t found after the samples are loaded. It’s a bit of a quick fix — I think version history loading needs some bigger reform (they need to not block display of the diff!), but it works well for now.

--- src/components/__tests__/page-details.test.jsx
+++ src/components/__tests__/page-details.test.jsx
@@ -29,7 +29,10 @@ describe('page-details', () => {
     samples = Object.values(samples).sort((a, b) => a.time < b.time ? 1 : -1);
 
     return Object.assign(Object.create(WebMonitoringDb.prototype), {
-      getPage: jest.fn().mockResolvedValue(simplePage),
+      getPage: jest.fn().mockResolvedValue({ ...simplePage }),
+      getVersion: jest.fn(id => Promise.resolve(
+        simplePage.versions.find(v => v.uuid === id)
+      )),
       getVersions: jest.fn().mockResolvedValue(simplePage.versions),
       sampleVersions: jest.fn().mockResolvedValue(samples)
     });
@@ -58,14 +61,40 @@ describe('page-details', () => {
       { context: { api: mockApi } }
     );
 
-    // Wait for mock APIs to have been called.
-    await mockApi.getPage.mock.results[0].value;
-    await mockApi.sampleVersions.mock.results[0].value;
     await timers.setTimeout(10);
+    expect(mockApi.getPage).toHaveBeenCalled();
+    expect(mockApi.sampleVersions).toHaveBeenCalled();
 
     expect(document.title).toBe('Scanner | http://www.ncei.noaa.gov/news/earth-science-conference-convenes');
 
     pageDetails.unmount();
     expect(document.title).toBe('Scanner');
+  });
+
+  it('gets versions missing from the sample', async () => {
+    const allVersions = simplePage.versions;
+    const mockApi = createMockApi();
+    const pageDetails = shallow(
+      <PageDetails
+        match={{
+          ...match,
+          params: {
+            ...match.params,
+            change: `${allVersions.at(-1).uuid}..${allVersions[0].uuid}`
+          }
+        }}
+      />,
+      { context: { api: mockApi } }
+    );
+
+    await timers.setTimeout(10);
+    expect(mockApi.getPage).toHaveBeenCalled();
+    expect(mockApi.sampleVersions).toHaveBeenCalled();
+    expect(mockApi.getVersion).toHaveBeenCalled();
+
+    // expect(document.title).toBe('Scanner | http://www.ncei.noaa.gov/news/earth-science-conference-convenes');
+    expect(pageDetails.exists()).toEqual(true);
+    // It should always have rendered *something.*
+    expect(pageDetails.get(0)).toBeTruthy();
   });
 });
