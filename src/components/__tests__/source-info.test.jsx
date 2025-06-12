793ae52f22acf60d2a15f53c0acb62aafa9235e1
Rob Brackett <rob@robbrackett.com>
2025-06-11 17:02:25 -0700

    Use React Testing Library for one suite
    As a test run, this replaces Enzyme with React Testing Library in the `source-info` test suite. Seems to work pretty well, although this is a simple(ish) case.
    
    This uses react-testing-library v12, which is far from current, but is the latest that works with React 16 (our current version) or 17.

--- src/components/__tests__/source-info.test.jsx
+++ src/components/__tests__/source-info.test.jsx
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import SourceInfo from '../source-info/source-info';
 
 describe('source-info', () => {
@@ -39,22 +39,28 @@ describe('source-info', () => {
   const pageUrl = 'https://19january2017snapshot.epa.gov/test-url';
 
   it('Renders only the Wayback calendar link if neither of the page versions have a view_url', () => {
-    const sInfo = shallow(<SourceInfo from={noViewUrl} to={noViewUrl} pageUrl={pageUrl} />);
-    expect(sInfo.text()).toBe('Wayback Machine calendar view');
-    const anchorTag = sInfo.find('a');
-    expect(anchorTag.length).toBe(1);
-    const { props: { href } } = anchorTag.get(0);
-    expect(href).toBe('https://web.archive.org/web/*/https://19january2017snapshot.epa.gov/test-url');
+    render(<SourceInfo from={noViewUrl} to={noViewUrl} pageUrl={pageUrl} />);
+
+    const link = screen.getByRole('link', { name: 'Wayback Machine calendar view' });
+    expect(link).toHaveAttribute('href', 'https://web.archive.org/web/*/https://19january2017snapshot.epa.gov/test-url');
+
+    const allLinks = screen.getAllByRole('link');
+    expect(allLinks).toHaveLength(1);
   });
 
   it('Renders the Wayback calendar link and previous/next versions with Wayback links if they are sourced from Wayback', () => {
-    const sInfo = shallow(<SourceInfo from={withViewUrl1} to={withViewUrl2} pageUrl={pageUrl} />);
-    expect(sInfo.text()).toBe('Wayback Machine calendar view | Wayback Machine previous page version | Wayback Machine next page version');
-    const anchorTags = sInfo.find('a');
-    expect(anchorTags.length).toBe(3);
-    const { props: { href : href1 } } = anchorTags.get(1);
-    const { props: { href: href2 } } = anchorTags.get(2);
-    expect(href1).toBe('http://web.archive.org/web/1111111/https://19january2017snapshot.epa.gov/test-url');
-    expect(href2).toBe('http://web.archive.org/web/22222/https://19january2017snapshot.epa.gov/test-url');
+    render(<SourceInfo from={withViewUrl1} to={withViewUrl2} pageUrl={pageUrl} />);
+
+    const allLinks = screen.getAllByRole('link');
+    expect(allLinks).toHaveLength(3);
+
+    const calendarLink = screen.getByRole('link', { name: 'Wayback Machine calendar view' });
+    expect(calendarLink).toHaveAttribute('href', 'https://web.archive.org/web/*/https://19january2017snapshot.epa.gov/test-url');
+
+    const aLink = screen.getByRole('link', { name: 'Wayback Machine previous page version' });
+    expect(aLink).toHaveAttribute('href', 'http://web.archive.org/web/1111111/https://19january2017snapshot.epa.gov/test-url');
+
+    const bLink = screen.getByRole('link', { name: 'Wayback Machine next page version' });
+    expect(bLink).toHaveAttribute('href', 'http://web.archive.org/web/22222/https://19january2017snapshot.epa.gov/test-url');
   });
 });
