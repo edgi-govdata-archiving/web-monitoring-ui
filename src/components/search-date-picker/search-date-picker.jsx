93773555c7ad51f31d1db60852181a070ec5895e
Rob Brackett <rob@robbrackett.com>
2025-06-19 18:33:00 -0700

    Make `SearchDatePicker` a function component
    It doesn't have any state anymore anyhow, but also the entire ecosystem is generally shifting towards hooks instead of class components.

--- src/components/search-date-picker/search-date-picker.jsx
+++ src/components/search-date-picker/search-date-picker.jsx
@@ -1,12 +1,11 @@
-import { Component } from 'react';
 import { DateTime } from 'luxon';
 import styles from './search-date-picker.css';
 
 /**
  * @typedef SearchDatePickerProps
- * @property {({startDate: DateTime, endDate: DateTime}) => void} onDateSearch
- * @property {DateTime} startDate
- * @property {DateTime} endDate
+ * @property {({startDate: DateTime|null, endDate: DateTime|null}) => void} onDateSearch
+ * @property {DateTime?} startDate
+ * @property {DateTime?} endDate
  */
 
 /**
@@ -14,50 +13,40 @@ import styles from './search-date-picker.css';
  * Once a date is selected, onDateSearch function is called with startDate and endDate values.
  * Dates can also be cleared out after selected.
  *
- * @class SearchDatePicker
- * @extends {Component}
  * @param {SearchDatePickerProps} props
  */
-export default class SearchDatePicker extends Component {
-  constructor (props) {
-    super(props);
-    this.state = { focusedInput: null };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange (event) {
+export default function SearchDatePicker ({ onDateSearch, startDate = null, endDate = null }) {
+  function handleChange (event) {
     const value = event.target.value ? DateTime.fromISO(event.target.value) : null;
-    this.props.onDateSearch({
-      startDate: this.props.startDate,
-      endDate: this.props.endDate,
+    onDateSearch({
+      startDate,
+      endDate,
       [event.target.name]: value,
     });
   }
 
-  render () {
-    return (
-      <>
-        <label className={styles.searchDateField}>
-          From date:
-          <input
-            type="date"
-            name="startDate"
-            value={this.props.startDate?.toISODate() ?? ''}
-            onChange={this.handleChange}
-            max={DateTime.now().toISODate()}
-          />
-        </label>
-        <label className={styles.searchDateField}>
-          To date:
-          <input
-            type="date"
-            name="endDate"
-            value={this.props.endDate?.toISODate() ?? ''}
-            onChange={this.handleChange}
-            max={DateTime.now().plus({ days: 1 }).toISODate()}
-          />
-        </label>
-      </>
-    );
-  }
+  return (
+    <>
+      <label className={styles.searchDateField}>
+        From date:
+        <input
+          type="date"
+          name="startDate"
+          value={startDate?.toISODate() ?? ''}
+          onChange={handleChange}
+          max={DateTime.now().toISODate()}
+        />
+      </label>
+      <label className={styles.searchDateField}>
+        To date:
+        <input
+          type="date"
+          name="endDate"
+          value={endDate?.toISODate() ?? ''}
+          onChange={handleChange}
+          max={DateTime.now().plus({ days: 1 }).toISODate()}
+        />
+      </label>
+    </>
+  );
 }
