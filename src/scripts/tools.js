0d4373b6715e102172c36929a22d8c2028a8b04b
Beckett Frey <83560790+BeckettFrey@users.noreply.github.com>
2026-02-26 00:10:27 -0600

    Handle malformed redirect data in some versions (#1173)
    Some old Versionista versions have malformed data in `source_metadata.redirects`, for example, an ISO timestamp string instead of a URL. `PageUrlDetails` (and `versionUrl()`) used to crash when handling this data, and this fixes the issue. Fixes #1173.

--- src/scripts/tools.js
+++ src/scripts/tools.js
@@ -7,6 +7,21 @@ export function removeNonUserTags (tags) {
 }
 
 /**
+ * Check whether a string is a valid URL.
+ * @param {string} url
+ * @returns {boolean}
+ */
+export function isValidUrl (url) {
+  try {
+    new URL(url);
+    return true;
+  }
+  catch {
+    return false;
+  }
+}
+
+/**
  * Get the URL that a version's body came from. If a version included redirects,
  * then its `url` property won't corresponded with the URL the response body
  * was rendered from. This gets the final target URL of all redirects, if there
@@ -22,8 +37,9 @@ export function versionUrl (version) {
       url = version.source_metadata.redirected_url;
     }
     else if (version.source_metadata.redirects) {
-      const redirects = version.source_metadata.redirects;
-      url = redirects[redirects.length - 1];
+      // Filter out malformed entries that aren't valid URLs.
+      // (Some old Versionista versions have bad data here.)
+      url = version.source_metadata.redirects.filter(isValidUrl).at(-1) || url;
     }
   }
 
